b796f3595606c0fe7d0e48c90a6195041850b972
herbayulet <sayaherbayu@gmail.com>
2023-12-22 15:40:31 +0700

    add connect to database for message

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,4 +1,5 @@
 const { remote } = require("webdriverio");
+const mysql = require("mysql2/promise"); // Import the mysql2 library
 
 const wdOpts = {
   hostname: process.env.APPIUM_HOST || "localhost",
@@ -13,72 +14,115 @@ const wdOpts = {
   },
 };
 
+const insertMessageIntoDatabase = async (message) => {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "testing_whatsapp",
+  });
+
+  try {
+    const [result] = await connection.execute(
+      "INSERT INTO history_chat (message) VALUES (?)",
+      [message]
+    );
+
+    // You can log the inserted row's information if needed
+    console.log("Inserted row:", result);
+
+    return result;
+  } finally {
+    await connection.end();
+  }
+};
+
 it("Run Test", async () => {
   let driver;
 
   try {
     driver = await remote(wdOpts);
 
-    // Menunggu sebentar setelah inisialisasi driver
-    await driver.pause(2000);
+    // Check if WebDriver session is started
+    if (!driver.sessionId) {
+      console.error("WebDriver session is not started. Exiting...");
+      return;
+    }
 
-    // Mengidentifikasi semua elemen TextView
+    // Wait for the Appium server to be ready
+    await driver.pause(5000);
+
+    // Identify all TextView elements
     const allTextViews = await driver.$$("//android.widget.TextView");
 
-    // Klik elemen dengan indeks 32
+    // Click on the TextView at index 5
     const targetIndex = 5;
-
     if (targetIndex < allTextViews.length) {
       await allTextViews[targetIndex].click();
-
-      // Menunggu sebentar setelah klik elemen
       await driver.pause(2000);
-      let el1 = await driver.$("~Search");
-      el1.click();
 
-      // Menunggu elemen "search_input" muncul
+      // Click on the "Search" element
+      const el1 = await driver.$("~Search");
+      await el1.click();
+
+      // Wait for the "input_layout" element to be displayed
       const el2 = await driver.$(
         'android=new UiSelector().resourceId("com.whatsapp:id/input_layout")'
       );
+      await el2.waitForDisplayed({ timeout: 5000 });
       await el2.click();
 
-      let el3 = await driver.$(
+      // Search for the contact "yusuf"
+      const el3 = await driver.$(
         'android=new UiSelector().resourceId("com.whatsapp:id/search_input")'
       );
       await el3.setValue("yusuf");
 
+      // Click on the search result
       const el4 = await driver.$(
         "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout"
       );
       await el4.click();
 
-      let el5 = await driver.$(
+      // Click on "text_entry_layout"
+      const el5 = await driver.$(
         'android=new UiSelector().resourceId("com.whatsapp:id/text_entry_layout")'
       );
-      el5.click();
-      let el6 = await driver.$(
+      await el5.click();
+
+      // Click on "input_layout"
+      const el6 = await driver.$(
         'android=new UiSelector().resourceId("com.whatsapp:id/input_layout")'
       );
-      el6.click();
-      let el7 = await driver.$(
+      await el6.click();
+
+      // Click on "entry" and set the value
+      const el7 = await driver.$(
         'android=new UiSelector().resourceId("com.whatsapp:id/entry")'
       );
-      el7.click();
-      el7.setValue("mas ucup gue lg testing dulu sorry");
+      await el7.click();
+      const pesan = "mas ucup gue lg testing dulu sorry, tp sekarang make DB";
+      await el7.setValue(pesan);
 
+      // Insert the message into the database
+      await insertMessageIntoDatabase(pesan);
+
+      // Wait for a moment before clicking the send button
       await driver.pause(2000);
+
+      // Click the send button
       const e8 = await driver.$(
         '//*[@resource-id="com.whatsapp:id/send_container"]'
       );
       await e8.click();
     } else {
-      console.error(`Elemen dengan indeks ${targetIndex} tidak ditemukan.`);
+      console.error(`Element with index ${targetIndex} not found.`);
     }
   } catch (error) {
-    console.error("Terjadi kesalahan:", error);
+    console.error("Error during execution:", error);
   } finally {
     if (driver) {
-      console.log("Menutup koneksi driver...");
+      console.log("Closing the driver connection...");
       await driver.deleteSession();
     }
   }
